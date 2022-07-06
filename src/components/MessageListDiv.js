c4643593703400944b83ff375464032c027811af
kursat <kursatsimsek@protonmail.ch>
2022-07-06 10:26:32 +0300

    live demo meeting message fixed

--- src/components/MessageListDiv.js
+++ src/components/MessageListDiv.js
@@ -114,7 +114,32 @@ const MessageListDiv = ({ type, chatInfo }) => {
                         <MessageBox
                           position={"left"}
                           type={"meeting"}
-                          meeting={meetingData}
+                          subject={new Date().toString()}
+                          title="Daily Meeting"
+                          date={new Date()}
+                          dateString={new Date().toDateString}
+                          collapseTitle="Info"
+                          participants={[
+                              {
+                                id: '1',
+                                title: 'Ahmet'
+                              },
+                              {
+                                id: '2',
+                                title: 'You'
+                              },
+                          ]}
+                          dataSource=
+                            {[
+                              {
+                                id: '1',
+                                avatar: 'https://avatars.githubusercontent.com/u/19151996?s=200&v=4',
+                                message: "Let's start the daily !",
+                                title: 'Daily Meeting',
+                                avatarFlexible: true,
+                                date: new Date(),
+                              }
+                            ]}
                         />,
                         meetingMessageExampleData.map((x, i) => {
                           return <MessageBox
